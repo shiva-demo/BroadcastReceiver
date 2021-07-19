3dc9c45151f5e5b49036dea6b286d5ddaa29cbec
DESKTOP-QVMHNLF\Xtrans <krishan@xtranssolutions.com>
2021-07-19 11:18:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.broadcastreceiver;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Nothing to see here.  Just launchers the fragment.
+ * the receiver and code is in MainFragment
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activit y_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new MainFragment()).commit();
+        }
+    }
+
+}
