6905804b6d16e8fb210bf1e01ffe01e99f8dd04a
Xtrans Solutions <krishan@xtranssolutions.com>
2021-10-13 11:09:37 +0530

    Demo

--- app/src/main/java/com/example/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activit y_main);
+        setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
